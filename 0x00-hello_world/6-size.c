c236be7afc96079d2204464bcd036b51bab143b2
Aliddeki Bryan <baliddeki@gmail.com>
2023-02-16 16:01:14 -0800

    function to print size of variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	char c;
+	int i;
+	long int b;
+	long long int f;
+	float d;
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a float: %lu byte(s\n)", (unsigned long)sizeof(d));
+	return (0);
+}
