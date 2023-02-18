876e42698fef5eb778bf96d72c5c44ba76b1a8f7
Aliddeki Bryan <baliddeki@gmail.com>
2023-02-18 12:00:17 +0300

    print positive and negative corrections

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
 /* 
  * main - Entry Poin
@@ -14,15 +13,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n>0)
+	if(n > 0)
 	{
 		printf("%d is positive\n", n);
 	} 
-       	if(n==0)
+       	if(n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-       	if(n<0)
+       	if(n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
