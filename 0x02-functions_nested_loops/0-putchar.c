8d5d1e6ebd88da2c479d97d7bc5500aaaf1fc98e
Aliddeki Bryan <baliddeki@gmail.com>
2023-02-21 18:08:49 +0300

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * print something
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+In this folder, I have covered functions and nested loops in C
