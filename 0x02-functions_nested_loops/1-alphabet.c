6c73cf76c2b6ccd1e6b313971e19fc28c3b7acb1
Aliddeki Bryan <baliddeki@gmail.com>
2023-02-24 21:59:16 +0300

    alphabet comment

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet - prints the alphabet from a to z
  *
- * Return: Always 0.
+ * Return: alphabet
  */
 
 void print_alphabet(void)
 {
 		char c = 'a';
+
 		while (c <= 'z')
 		{
 			_putchar(c);
