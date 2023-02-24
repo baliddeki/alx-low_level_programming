77680cce2ed33040193fe498b6270f953e45baa3
Aliddeki Bryan <baliddeki@gmail.com>
2023-02-24 22:20:29 +0300

    lower comment

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,15 @@
 #include "main.h"
 
+/**
+ * _isalpha - checks for alphabetic chracter
+ * @c: ay character
+ * Return: 1 (Sucess), 0(Failure)
+ */
+
 int _isalpha(int c)
 {
 	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }	
