70b0b3dd819e4a89134a526b6ca4f2e26e44aa20
Aliddeki Bryan <baliddeki@gmail.com>
2023-02-24 22:01:48 +0300

    comments

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,9 +2,11 @@
 
 /**
  *
- * function - prints the alphabet 10 times
- *
- **/
+ * print_alphabet_x10 - prints the alphabet 10 times
+ * 
+ * Return: void
+ */
+
 void print_alphabet_x10(void)
 {
 	int i; 
