b26dda073665453b392b358199dc41803b2fd7d1
Aliddeki Bryan <baliddeki@gmail.com>
2023-02-24 13:20:09 +0300

    multiply table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int r;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int r = i*j;
+			if (j = 0)
+			{
+				_putchar('0');
+			} else if (r < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(r + '0');
+			} else 
+			{	_putchar(',');
+				_putchar(' ');
+				_putchar(r / 10 + '0');	
+				_putchar(r % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
