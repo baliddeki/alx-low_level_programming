f75535f528c7631c3ef3202d5773904d76820002
Aliddeki Bryan <baliddeki@gmail.com>
2023-02-25 03:41:22 +0300

    correction

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * print_last_digit - prints the last digit of anu number
+ * @n: any number
+ * Return: 1
+ */
+
 int print_last_digit(int n)
 {
 	int l = n % 10;
+	
 	if (l < 0)
 	{
 		l = -l;
@@ -11,4 +18,3 @@ int print_last_digit(int n)
 	_putchar(l + '0');
 	return l;
 }
-	
