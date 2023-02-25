0b865453dd74d45c8cb274d077a238d50ddab01e
Aliddeki Bryan <baliddeki@gmail.com>
2023-02-25 04:16:36 +0300

    print upto 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,13 +10,13 @@ void print_to_98(int n)
 {
 	for (n = n; n <= 97; n++)
 	{
-		return (n);
+		_putchar(n);
 		_putchar(',');
 		_putchar(' ');
 	}
 	for (n = n; n >= 99; n--)
 	{
-		return (n);
+		_putchar(n);
 		_putchar(',');
 		_putchar(' ');
 	}
