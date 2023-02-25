5e42658047cb6c419d5d366ab86c01a8afbab057
Aliddeki Bryan <baliddeki@gmail.com>
2023-02-25 03:35:47 +0300

    correction

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
+
+/**
+ * _abs - function to print absolute number of an integer
+ * @n: any positive or negative integre
+ * Return: absolute number
+ */
+
 int _abs(int n)
 {	
 	int c = abs(n);
-	return c;
+	return (c);
 }
