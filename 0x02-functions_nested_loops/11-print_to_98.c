8e88b6099e091346397d15ca478ba6d8d21bdee5
Aliddeki Bryan <baliddeki@gmail.com>
2023-02-25 04:14:52 +0300

    print upto 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_to_98 - function prints all natural numbers from n to 98
+ * @n: any integer number
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	for (n = n; n <= 97; n++)
+	{
+		return (n);
+		_putchar(',');
+		_putchar(' ');
+	}
+	for (n = n; n >= 99; n--)
+	{
+		return (n);
+		_putchar(',');
+		_putchar(' ');
+	}
+}
