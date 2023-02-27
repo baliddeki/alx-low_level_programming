a67ea6a132f7ec38f6ae123d59680c8b7fab0d7a
Aliddeki Bryan <baliddeki@gmail.com>
2023-02-27 23:24:26 +0300

    update value using pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,15 +3,11 @@
 /**
  * reset_to_98 - function takes a pointer as a paramater
  * it then updates the value to 98
- * n: any integer
+ * n - any integer
  * Return: void
  */
 
 void reset_to_98(int *n)
 {
-	int a;
 	*n = 98;
-	n = &a;
-
-	_putchar(a);
 }
