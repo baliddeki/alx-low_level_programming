c3bcc5e21c5481417bb28715c32e7fd571b9caa8
Aliddeki Bryan <baliddeki@gmail.com>
2023-02-28 15:16:17 +0300

    print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of the string
+ * @str: any string
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int len = strlen(str);
+       	int start = len % 2 == 0 ? len / 2 : (len - 1) / 2 + 1;
+}
