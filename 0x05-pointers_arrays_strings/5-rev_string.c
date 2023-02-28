271bbc06d245c5d7b625271944e8c307148a9543
Aliddeki Bryan <baliddeki@gmail.com>
2023-02-28 14:29:10 +0300

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_string - function reverses a string
+ * @s: any string
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	while (*(s + len))
+		len++;
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = *(s + i);
+		*(s + i) = *(s + len - 1 - i);
+		*(s + len - 1 - i) = temp;
+	}
+}
