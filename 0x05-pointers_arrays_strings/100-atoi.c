fe01ec2cc09bdb603b4fece298714e3f1b7558cb
Aliddeki Bryan <baliddeki@gmail.com>
2023-03-01 05:20:21 +0300

    convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,14 +10,15 @@
 
 int _atoi(char *s)
 {
-	int result = 0;
 	int sign = 1;
+	int result = 0;
+	int digits_seen = 0;
 
 	while (*s == ' ')
 	{
 		s++;
 	}
-	if ((*s == '-') || (*s == '+'))
+	while ((*s == '+') || (*s == '-'))
 	{
 		if (*s == '-')
 		{
@@ -27,12 +28,17 @@ int _atoi(char *s)
 	}
 	while ((*s >= '0') && (*s <= '9'))
 	{
-		if (result > (INT_MAX - (*s - '0')) / 10)
+		digits_seen = 1;
+		if ((result > INT_MAX / 10) || ((result == INT_MAX / 10) && (*s - '0' > INT_MAX % 10)))
 		{
 			return (sign > 0 ? INT_MAX : INT_MIN);
 		}
-		result = result * 10 + (*s - '0');
+		result = (result * 10) + (*s - '0');
 		s++;
+	}
+	if (!digits_seen)
+	{
+	return (0);
 	}
 	return (result * sign);
 }
