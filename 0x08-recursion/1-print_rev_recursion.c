5cb54e0045afa385e22da66f2be5c72a35c4edbd
Aliddeki Bryan <baliddeki@gmail.com>
2023-03-09 16:42:42 +0300

    reverse string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,17 +9,7 @@
 
 void _print_rev_recursion(char *s)
 {
-	int i = strlen(s);
-	int mid = i / 2;
-	char c;
-	int j = 0;
-
-	c = *(s + j);
-	*(s + j) = *(s + i - j - 1);
-	*(s + i - j -1) = c;
-	_putchar(c);
-	j++;
-
-	if (j < mid)
-		_print_rev_recursion(s);
+	if (*s != '\0')
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 }
