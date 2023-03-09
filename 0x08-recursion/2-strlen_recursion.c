30727dad8616a42362bbb316fc7e9bfd8e2f2460
Aliddeki Bryan <baliddeki@gmail.com>
2023-03-09 16:57:23 +0300

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - calculate length of string
+ * @s: any string
+ * Return: integer
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = strlen(s);
+	return (i);
+}
