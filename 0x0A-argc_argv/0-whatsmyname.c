bab611cc34df8af0a6c71aca8ab5655a0056a74e
Aliddeki Bryan <baliddeki@gmail.com>
2023-03-15 18:42:51 +0300

    file name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,13 +7,13 @@
  * @*argv: array
  */
 
+void print_name(char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
+
 int main(int argc, char  *argv[])
 {
 	print_name(argv);
 	return (0);
-}
-
-void print_name(char *argv[])
-{
-	printf("%s\n", argv[0]);
 }
